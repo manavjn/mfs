b21f0ddfb2b4d577145800b510180fa2b7fe5ce3
Manav Nirmalraj <mnirmalraj@unomaha.edu>
2019-02-04 23:31:26 -0600

    additional files and changes for Heroku

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -132,6 +132,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
